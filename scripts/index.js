da236b94f04ef4a3b8ad3edc25554f42e33ada0e
DrooID <71921743+DrooID713@users.noreply.github.com>
2023-10-04 14:33:23 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-import { initialCards, classData } from './initialData.js';
+import { initialCards, classData } from './initialCards.js';
 
 const profile = document.querySelector('.profile');
 const profileName = profile.querySelector('.profile__name');
